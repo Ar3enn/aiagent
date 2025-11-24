9a2f0709d02a91746b46f2722cac3e1df19f1067
Ar3enn <ar3enn@gmail.com>
2025-11-24 22:50:37 +0300

    added basic functionality

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 def main():
-    print("Hello from aiagent!")
+    if len(sys.argv) < 2:
+        print("Error: Please provide a prompt as a command-line argument.")
+        sys.exit(1)
+    prompt_text = sys.argv[1]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents= prompt_text
+        )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
