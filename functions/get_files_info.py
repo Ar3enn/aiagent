7591beb1d9e9e506bb8e2614fb07e310b96b9bb8
Ar3enn <ar3enn@gmail.com>
2025-11-25 17:04:39 +0300

    added calculator app and get_file_info function

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+def get_files_info(working_directory,directory="."):
+    
+    working_directory_abs = os.path.abspath(working_directory)
+    full_dir = os.path.join(working_directory_abs,directory)
+    full_dir = os.path.abspath(full_dir)
+    
+    try:
+        
+        if not full_dir.startswith(working_directory_abs):
+            return print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        elif not os.path.isdir(full_dir):
+            return print(f'Error: "{directory}" is not a directory')
+    
+        for item in os.listdir(full_dir):
+            item_name = item
+            item = os.path.join(full_dir,item)
+            print(f"- {item_name}: file_size:{os.path.getsize(item)} bytes, is_dir={os.path.isdir(item)}")
+            
+    except Exception as e:
+        return f"Error: {e}"
+        
+   
+    
