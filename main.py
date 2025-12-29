3c5068f0a29330f60e4b8abacab97c31569e2834
Ar3enn <ar3enn@gmail.com>
2025-12-29 06:30:22 +0300

    added function calling capability

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 def main():
+    
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     
     if len(sys.argv) < 2:
         print("Error: Please provide a prompt as a command-line argument.")
@@ -20,11 +31,16 @@ def main():
     ]
     
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
-        contents=messages
+        model="gemini-2.5-flash", 
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
         )
     
-    print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
     
     if len(sys.argv) >= 3:
         if sys.argv[2] == "--verbose":
